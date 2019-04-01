557f7430740e82c9c8d0e2fd6954e2cba5158ba5
CheonSeounghyun <ehwjsx@gmail.com>
2019-04-01 16:13:08 +0900

    fixed untracked files

--- code/capston_design/mainpage/views.py
+++ code/capston_design/mainpage/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import  render
+
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("20대 남녀 코디 추천 웹사이트 ")
+    return render(request,'mainpage/templates/index.html')
+
+
