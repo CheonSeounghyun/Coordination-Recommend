f5bc4d802608e35da81b8bac5382ab9b91bd4a54
CheonSeounghyun <ehwjsx@gmail.com>
2019-05-16 23:23:32 +0900

    home

--- code/capston_design/settings.py
+++ code/capston_design/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'capston_design.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,8 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'recommend','static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
