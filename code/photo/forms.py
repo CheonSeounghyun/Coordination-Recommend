aeeddaa79b59820b546788ebf1c13d795b35d3b4
CheonSeounghyun <ehwjsx@gmail.com>
2019-06-12 14:21:24 +0900

    doc

--- code/photo/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms.models import inlineformset_factory
-
-from .models import Album, Photo
-
-
-
-PhotoInlineFormSet = inlineformset_factory(Album, Photo,
-    fields = ['image', 'title', 'description'],
-    extra = 2)
